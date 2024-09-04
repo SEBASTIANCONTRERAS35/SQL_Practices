a7dfa196aca2e972836dd7a1f29525e1c9c984be
BICHOTEE <emilio3547@outlook.es>
2024-09-04 15:01:59 -0600

    First commit

---
+++ RefiningSelections.sql
@@ -0,0 +1,96 @@
+
+use booksShop;
+create table books(
+title varchar(50),
+author_fname varchar(50),
+author_lname varchar(50),
+released_year int,
+stock_quantity int,
+pages int
+);
+INSERT INTO books
+    (title, author_fname, author_lname, released_year, stock_quantity, pages)
+    VALUES ('10% Happier', 'Dan', 'Harris', 2014, 29, 256), 
+           ('fake_book', 'Freida', 'Harris', 2001, 287, 428),
+           ('Lincoln In The Bardo', 'George', 'Saunders', 2017, 1000, 367);
+           SELECT author_lname FROM books;
+ 
+SELECT DISTINCT author_lname FROM books;
+ 
+SELECT author_fname, author_lname FROM books;
+ 
+SELECT DISTINCT CONCAT(author_fname,' ', author_lname) FROM books;
+ 
+SELECT DISTINCT author_fname, author_lname FROM books;
+SELECT * FROM books
+ORDER BY author_lname;
+ 
+SELECT * FROM books
+ORDER BY author_lname DESC;
+ 
+SELECT * FROM books
+ORDER BY released_year;
+
+SELECT book_id, author_fname, author_lname, pages
+FROM books ORDER BY 2 desc;
+ 
+SELECT book_id, author_fname, author_lname, pages
+FROM books ORDER BY author_lname, author_fname;
+
+SELECT title FROM books LIMIT 3;
+ 
+SELECT title FROM books LIMIT 1;
+ 
+SELECT title FROM books LIMIT 10;
+ 
+SELECT * FROM books LIMIT 1;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 5;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 1;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 14;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 0,5;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 0,3;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 1,3;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 10,1;
+ 
+SELECT * FROM tbl LIMIT 95,18446744073709551615;
+ 
+SELECT title FROM books LIMIT 5;
+ 
+SELECT title FROM books LIMIT 5, 123219476457;
+ 
+SELECT title FROM books LIMIT 5, 50;
+
+-- Wildcards
+SELECT title, author_fname, author_lname, pages 
+FROM books
+WHERE author_fname LIKE '%da%';
+ 
+SELECT title, author_fname, author_lname, pages 
+FROM books
+WHERE title LIKE '%:%';
+ 
+SELECT * FROM books
+WHERE author_fname LIKE '____';
+ 
+SELECT * FROM books
+WHERE author_fname LIKE '_a_';
+
+select title as titulo, pages as paginas from books order by pages limit 32,32;
+select concat(title, ' ',released_year) as libros_recientes from books order by released_year desc limit 3;
+select title, author_lname from books where author_lname like ('% %');
+select title,author_lname from books order by author_lname,title;
+SELECT CONCAT('MY FAVORITE AUTHOR IS ', UPPER(author_fname),' ',UPPER(author_lname),'!') AS yell FROM books ORDER BY author_lname;
